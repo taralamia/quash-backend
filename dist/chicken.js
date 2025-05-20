7034ebdc3ffad5960eb55e5d05527ac11ce9ae6a
taralamia <79480117+taralamia@users.noreply.github.com>
2025-05-20 22:36:08 +0600

    chore: project setup with typescript #1

---
+++ dist/chicken.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Chicken = void 0;
+class Chicken {
+    cluck() {
+        console.log("Cluck");
+    }
+}
+exports.Chicken = Chicken;
+//# sourceMappingURL=chicken.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Chicken } from "./chicken";
+console.log("Hello Typescript");
+const chicken = new Chicken();
+//chicken.cluck();
