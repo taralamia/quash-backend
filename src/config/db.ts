d36e93b731b2198e00019ce38899403f375e33fe
taralamia <79480117+taralamia@users.noreply.github.com>
2025-06-03 05:59:40 +0600

    chore: setup prettier-eslint-database connection

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const db_url: string = process.env.DB_URL as string;
+ const port : string = process.env.PORT as string;
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(db_url);
+        console.log("Connected to the db");
+    } catch (err) {
+        console.error("Failed to connect to the db", err);
+    }
+};
+
+export  {port,connectToDatabase};
---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,9 @@
+/*const User = require('../models/User');
+
+exports.findById = async (id) => {
+    return await User.findById(id);
+};
+
+exports.save = async (user) => {
+    return await user.save();
+};*/
