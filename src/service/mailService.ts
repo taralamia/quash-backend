e6c4518571ecac09ec7930e19dae842ba41b9fb0
unknown <taralamia69@gmail.com>
2025-08-01 00:04:31 +0600

    chore: Move the verification hmtl code to a separate folder #3

--- src/service/mailService.ts
+++ src/service/mailService.ts
@@ -1,22 +1,17 @@
 import { transporter } from "../middleware/users/mailer";
+import { env } from "../constants/envConfig";
+import { readFileSync } from "fs";
+import path from "path";
 export class MailService {
   async sendVerificationEmail(params: { email: string; code: string }) {
+    const templatePath = path.join(__dirname, "../templates/verificationEmail.html");
+    let htmlContent = readFileSync(templatePath, "utf-8");
+    htmlContent = htmlContent.replace("{{code}}", params.code);
     const mailOptions = {
-      from: `"A1A Car Wash" <${process.env.EMAIL_FROM}>`,
+      from: `"A1A Car Wash" <${env.EMAIL_FROM}>`,
       to: params.email,
       subject: "Verify Your Email",
-      html: `
-        <div style="font-family: Arial, sans-serif; padding: 20px;">
-          <h2>Welcome to A1A Car Wash 🚗</h2>
-          <p>Hi there,</p>
-          <p>Thank you for registering. Please verify your email using the following verification code:</p>
-          <div style="font-size: 24px; font-weight: bold; margin: 20px 0;">${params.code}</div>
-          <p>This code will expire in 24 hours.</p>
-          <p>If you did not sign up, you can safely ignore this message.</p>
-          <br />
-          <p>– The A1A Car Wash Team</p>
-        </div>
-      `,
+      html: htmlContent,
     };
     await transporter.sendMail(mailOptions);
   }
